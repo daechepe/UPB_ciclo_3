e584e5ac9cb1e3f82be78b383d9601cabe8c07ba
Zero Engel <87742451+Zer0engel@users.noreply.github.com>
2021-10-18 12:28:52 -0500

    Delete crear_bd.sql

--- SQL/crear_bd.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE upb_sprint4;
-use upb_sprint4;
-CREATE TABLE usuarios(
-id integer NOT NULL AUTO_INCREMENT,
-usuario VARCHAR(256) NOT NULL,
-contrasena VARCHAR(256) NOT NULL,
-PRIMARY KEY(id)
-)
